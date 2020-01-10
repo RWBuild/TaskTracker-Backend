e717a5fa07b57a213127f1fa759a2ab764b28886
promesse kayenga <promskakaprodo45@gmail.com>
2020-01-10 09:52:49 +0200

    creation of model-controller-migration and murugo user seed

---
+++ database/migrations/2020_01_09_145619_create_entries_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateEntriesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('entries', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->integer("task_id");
+            $table->string("entry_type");
+            $table->time("entry_time");
+            $table->time("entry_duration");
+            $table->timestamps();
+        });
+    }
+   
+    
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('entries');
+    }
+}
