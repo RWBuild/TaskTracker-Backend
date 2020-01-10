7bc7837c3d06d896f00981673b9eafaf8ce9cb9f
promesse kayenga <promskakaprodo45@gmail.com>
2020-01-10 12:19:53 +0200

    implemented the authentication endpoint

--- app/Http/Controllers/ApiControllers/Auth/LoginController.php
+++ app/Http/Controllers/ApiControllers/Auth/LoginController.php
@@ -2,13 +2,56 @@
 
 namespace App\Http\Controllers\ApiControllers\Auth;
 
+use App\User;
+use App\MurugoUser;
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
 
 class LoginController extends Controller
 {
-    public function login ()
+    public function login (Request $request)
     {
         
+        $this->validate($request,[
+            'murugo_user_id' => 'required'
+        ]);
+
+        $murugo_user = MurugoUser::where('murugo_user_id', $request->murugo_user_id)
+                        ->first();
+        
+       //check if murugo user id exists in db                
+       if (!$murugo_user) {
+           return response()->json([
+               'success' => false,
+               'message' => 'User not allowed'
+           ]);
+       }
+       
+       $user = $murugo_user->user;
+       //check if the murugo user is connected to timeTracker user
+       if (!$user) {
+           
+           $this->validate($request,[
+               'names' => 'required',
+               'email' => 'required|email|unique:users',
+               'avatar' => 'required'
+           ]);
+           $user = User::create([
+                'names' => $request->names,
+                'email' => $request->email,
+                'avatar' => $request->avatar
+           ]);
+       }
+
+       $generated_token = $user->createToken('authToken');
+
+       return response()->json([
+           'success' => true,
+           'message' => 'Successfully identified',
+           'user' => $user,
+           'access_token' => $generated_token->accessToken,
+           'token_expires_at' => $generated_token->token->expires_at
+       ]);
+       
     }
 }
--- routes/api.php
+++ routes/api.php
@@ -17,4 +17,4 @@ Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
 
-Route::get("/login","ApiControllers\Auth\LoginController@login");
+Route::post("/login","ApiControllers\Auth\LoginController@login");
