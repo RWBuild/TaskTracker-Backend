8aeba5b801b8a90fc5886a5987003df36de9a49f
Mikjean <nzamic@gmail.com>
2020-01-10 11:37:03 +0200

    protected fillable var in projects and records

--- app/Record.php
+++ app/Record.php
@@ -6,5 +6,14 @@ use Illuminate\Database\Eloquent\Model;
 
 class Record extends Model
 {
-    //
+    protected $fillable = [
+        'id',
+         'project_id', 
+         'user_id', 
+         'name', 
+         'description', 
+         'is_curent', 
+         'is_paused', 
+         'is_completed'
+    ];
 }
