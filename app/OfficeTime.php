050e54c4fe6ef33a52e446bace75fb3916f3baae
Mikjean <nzamic@gmail.com>
2020-01-10 11:54:11 +0200

    updated the office time filleable fields and updated records

--- app/OfficeTime.php
+++ app/OfficeTime.php
@@ -6,5 +6,11 @@ use Illuminate\Database\Eloquent\Model;
 
 class OfficeTime extends Model
 {
-    //
+    protected $fillable = [
+        'checkin_time',
+        'checkout_time',
+        'duration',
+        'user_id'
+
+    ];
 }
--- app/Record.php
+++ app/Record.php
@@ -7,13 +7,12 @@ use Illuminate\Database\Eloquent\Model;
 class Record extends Model
 {
     protected $fillable = [
-        'id',
-         'project_id', 
-         'user_id', 
-         'name', 
-         'description', 
-         'is_curent', 
-         'is_paused', 
-         'is_completed'
-    ];
+        'project_id', 
+        'user_id', 
+        'name', 
+        'description', 
+        'is_curent', 
+        'is_paused', 
+        'is_completed'
+   ];
 }
