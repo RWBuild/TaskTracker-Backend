c8a6632c930aa09921f8edd349397a66e0e3d3d7
promesse kayenga <promskakaprodo45@gmail.com>
2020-01-10 14:53:08 +0200

    added radius column to officetimes table

--- database/migrations/2020_01_09_145703_create_locations_table.php
+++ database/migrations/2020_01_09_145703_create_locations_table.php
@@ -17,6 +17,7 @@ class CreateLocationsTable extends Migration
             $table->bigIncrements('id');
             $table->double("longitude");
             $table->double("latitude");
+            $table->double("radius");
             $table->timestamps();
         });
     }
