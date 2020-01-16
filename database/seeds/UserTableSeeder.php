9bf74a296205fa5473cdbc0932ddfd869ead186d
promesse kayenga <promskakaprodo45@gmail.com>
2020-01-16 14:08:30 +0200

    fixed murugo_user seeder

--- database/seeds/UserTableSeeder.php
+++ database/seeds/UserTableSeeder.php
@@ -12,36 +12,24 @@ class UserTableSeeder extends Seeder
      */
     public function run()
     {
-        $this->truncateTable();
 
         $murugo_user_list = explode(',',env('MURUGO_USER_SAMPLE'));
-        $user_names = ['doctor prodoo',"cedric", "fred geek","michecl yes"];
-        $i = 0;
+
+        foreach ($murugo_user_list as $murugo_user_id) {
+          
+            $murugoUser = App\MurugoUser::where('murugo_user_id',$murugo_user_id)
+            ->delete();
+            
+        }        
 
         foreach ($murugo_user_list as $murugo_user_id) {
           
            $murugoUser = App\MurugoUser::create([
             'murugo_user_id' => $murugo_user_id
            ]);
-
-           $user = App\User::create([
-            'names' => $user_names[$i],
-            'email' => Str::random(10).'@gmail.com',
-            'avatar' => Str::random(10).'png'
            
-           ]);
-
-           $murugoUser->user_id = $user->id;
-           $murugoUser->save();
-
-           $i++;
         }
         
     }
 
-    public function truncateTable()
-    {
-        \App\MurugoUser::truncate();
-        \App\User::truncate(); 
-    }
 }
