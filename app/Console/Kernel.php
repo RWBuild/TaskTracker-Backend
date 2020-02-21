11f234e8a9fe1b6a8c18b59286f7f1b1f33a6613
promesse kayenga <promskakaprodo45@gmail.com>
2020-02-21 16:58:51 +0200

    fixed auto checkout to run only at midinight

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -25,8 +25,8 @@ class Kernel extends ConsoleKernel
     protected function schedule(Schedule $schedule)
     {
         $schedule->command('checkout:users')
-                ->everyMinute();
-                //  ->daily()->timezone('Africa/Cairo'); // set the schedule to run on our local time
+                     ->daily()
+                     ->timezone('Africa/Cairo'); // set the schedule to run on our local time
     }
 
     /**
